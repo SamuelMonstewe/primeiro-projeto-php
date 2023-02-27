acc5f48297ce3d56cc6805b32f3c1f0a1c6937df
Samuel Elias <99209614+SamuelMonstewe@users.noreply.github.com>
2023-02-27 16:40:03 -0300

    meu-primeiro-projeto-php

---
+++ primeiro-projeto/index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <script></script>
+</head>
+<body>
+    
+    <h1>
+        <?php
+         "ola mundo";
+        ?>
+    </h1>    
+
+    <h2> Resultado de 1+1 = 
+        <?php
+        echo(1+1)
+        ?>
+    </h2>
+
+    <h2> Resultado de 1-1 = 
+    <?php
+        echo(1-1)
+        ?>
+    </h2>
+
+    <h2> Resultado de 1*1 = 
+    <?php
+        echo(1*1)
+        ?>
+    </h2>
+
+    <h2> Resultado de 1/1 = 
+    <?php
+        echo(1/1)
+        ?>
+    </h2>
+
+</body>
+</html>
